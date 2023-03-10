022fb2c3492f8c5f249400b31bc9c761274b5132
MarthenTen <tenouyemarten@gmail.com>
2023-03-10 10:48:32 +0900

    coding php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+	$judul = "hello world !!! ";
+	$bilangan = 10;
+	$nama = "Marthen";
+
+ ?>
+
+
+<html>
+
+<head>
+
+<title>php day 00</title>
+
+</head>
+<body>
+
+	<h1>PHP day 00 -FILE PHP</h1>
+	<h2> <?php echo   $judul; ?> </h2>
+	<h2> <?= $bilangan; ?> </h2>
+<h2> <?php echo $judul .   $nama; ?> </h2>
+</body>
+</html>
